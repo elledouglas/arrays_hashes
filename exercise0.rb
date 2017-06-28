c8c033891d20ba460f7ae3d0f2bec7441e0db95c
Kathleen <kathleenmcclay@gmail.com>
2017-06-28 15:05:05 -0400

    Exercise 6 complete

--- exercise0.rb
+++ exercise0.rb
@@ -56,7 +56,7 @@ coin_flip.reverse
  puts city[:texas]
 
  #5
- fav_artist.each do |artist|
+ fav_artist.map do |artist|
    puts "I think #{artist} is great"
  end
 
@@ -76,4 +76,54 @@ siblings_age.reverse
 
 #4
 movie[:Beauty_and_the_beast] = 1991, 2017
-puts movie 
+puts movie
+
+#exercise 5
+siblings_age = [30, 60, 50, 10]
+
+puts siblings_age if < 50
+
+
+#3
+coin_flip = ["heads", "heads", "heads", "tails", "heads"]
+coin_flip.count
+
+#4
+
+siblings_age = [30, 60, 50, 10]
+
+siblings_age.each do |value|
+  siblings_age += 1
+return siblings_age
+end
+
+#5
+fav_colors = ["blue", "red", "green", "pink", "yellow"]
+fav_colors << 'rainbow'
+
+
+#exercise 6
+movies_year = {1991 => ['The Matrix', 'Star Wars', 'The Mummy'],
+  2009 => ['Avatara', 'Star Trek', 'District 9'],
+  2019 => ['How to Train Your Dragon 3', 'Toy Story', 'Star Wars']
+}
+
+#2
+number = [1, 2, 3], [4, 5, 6], [7, 8, 9], [0]
+
+#3
+countries = [{:Tokyo =>'Asia', :island => 'no'}], [{:Canada =>'North America', :island => 'no'}], [{:Sweden =>'Europe', :island => 'no'}]
+
+}
+  Canada {
+
+    :continent =>'North America',
+    :island => 'no'
+  }
+
+  Tokyo {
+
+  :continent =>'Asia'
+  :island => 'no'
+
+}
