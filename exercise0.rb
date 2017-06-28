9f0a3119825dd5c38ae63cc13c074e5ab22f6b31
Kathleen <kathleenmcclay@gmail.com>
2017-06-28 08:31:47 -0400

    Exercise 1 complete

--- exercise0.rb
+++ exercise0.rb
@@ -1,5 +1,5 @@
 fav_colors = ["blue", "red", "green", "pink", "yellow"]
-siblings_age = [20, 30, 50 60]
+siblings_age = [30, 60, 50, 10]
 coin_flip = ["heads", "heads", "heads", "tails", "heads"]
 fav_artist = ["Sam Cooke", "Brothers Osborne", "Don Henley"]
 
@@ -10,7 +10,7 @@ dictionary {
 }
 
 movies {
-  :pride_and_prejudice => 2005,
+  :pride_and_prejudice=>2005,
   :man_on_fire => 2006,
   :bourne_identity => 2008
 }
@@ -27,3 +27,18 @@ siblings {
   :Brian => 60,
   :Andrea => 50
 }
+
+#Exercise 1
+#1
+puts coin_flip
+#2
+puts #or print
+fav_colors[0]
+
+#3
+siblings_age.sort
+#4
+ siblings_age << 0
+
+ #5
+ # movies isnt working COME BACK 
