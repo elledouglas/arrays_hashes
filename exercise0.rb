e81547157ac41c67c732dc51ea3b5fc5c64da152
Kathleen <kathleenmcclay@gmail.com>
2017-06-28 08:09:38 -0400

    First Commit Exercise 0 complete

---
+++ exercise0.rb
@@ -0,0 +1,29 @@
+fav_colors = ["blue", "red", "green", "pink", "yellow"]
+siblings_age = [20, 30, 50 60]
+coin_flip = ["heads", "heads", "heads", "tails", "heads"]
+fav_artist = ["Sam Cooke", "Brothers Osborne", "Don Henley"]
+
+dictionary {
+  :Bitmaker => "A 12 week immersive coding bootcamp"
+  :perish => "A gradual, nonviolent death "
+  :taciturnly => "Reserved and uncommunicative"
+}
+
+movies {
+  :pride_and_prejudice => 2005,
+  :man_on_fire => 2006,
+  :bourne_identity => 2008
+}
+
+cities {
+  :Nashville => 1,000,000,
+  :Vancouver => 3,000,000,
+  :Tokyo => 5,000,000
+}
+
+siblings {
+  :Kyle => 30,
+  :Kaitlyn => 20,
+  :Brian => 60,
+  :Andrea => 50
+}
